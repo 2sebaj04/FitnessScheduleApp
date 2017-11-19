8b71f537aa2cbb40c57462d8385eaec39019d561
Sebastiao97 <jasonsebastiao@gmail.com>
2017-11-19 23:11:47 +0000

    Initial commit

---
+++ app/src/main/java/com/example/sebastiao97/fitnessscheduleapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.sebastiao97.fitnessscheduleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean OnOptionsItemSelected(MenuItem item) {
+
+        return true;
+    }
+
+}
